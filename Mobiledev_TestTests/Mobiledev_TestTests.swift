d5db228c38f722f12df971d046e92e1a4adce1c9
SwagMam <bagusimamprakoso58@gmail.com>
2025-01-16 14:49:49 +0700

    hapus folder

---
+++ Mobiledev_TestTests/Mobiledev_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mobiledev_TestTests.swift
+//  Mobiledev_TestTests
+//
+//  Created by Mas Bagus on 15/01/25.
+//
+
+import Testing
+@testable import Mobiledev_Test
+
+struct Mobiledev_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
